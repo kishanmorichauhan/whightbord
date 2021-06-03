676b7c52a7257b334a58a83afd610dda950959b6
kishan morichauhan <kishanmorichauhan@gmail.com>
2021-06-03 09:41:01 +0530

    -

---
+++ app/src/main/java/com/example/whitebord/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.whitebord;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    CanvasView canvasView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        canvasView = findViewById(R.id.canvas);
+    }
+    public  void clearCanvas(View v){
+        canvasView.clearCanvas();
+    }
+
+}
